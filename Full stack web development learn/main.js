288363b5d97da83e9bf2650c41af9d40451224b0
Samuel Sokari Brown <Sammybryan21200915@gmail.com>
2025-12-27 22:27:52 +1100

    Add files via upload

---
+++ Full stack web development learn/main.js
@@ -0,0 +1,110 @@
+/* document.getElementById('count-el').innerText = 5;
+let count = 0;
+console.log(count);
+
+let myAge = 16;
+console.log(myAge);
+let btn = document.getElementsById('btn');
+let countone = document.getElementsById('count-el');
+btn = countone;
+console.log(btn)*/
+
+// Javascript for a counter app
+// let saveEl = document.getElementById("saveEl")
+// let countEl = document.getElementById("h2")
+// let count = 0;
+// function addone() { 
+//    count += 1;
+//    document.getElementById('h2').innerText = count;
+// }
+
+// let countminus = count;
+// function MinusOne() {
+//    count += - 1;
+//    document.getElementById('h2').innerText = count;
+// }
+
+// function save() {
+//    let countStr = count + " - "
+//    saveEl.textContent += countStr
+// }
+// save()
+
+// // Strings
+// let username = "perez"
+// let message = "You have three new notifications"
+// let messageToUser = message + " , " + username + " !"
+// console.log(messageToUser)
+
+// // escape
+
+// let wish = "I\'d love to see the show \"the lion king\"."
+// console.log(wish)
+
+// document.getElementById is also known as THE DOM meaning document object model
+
+// let firstname = 'samuel '
+// let lastname = 'brown'
+// let fullname = firstname + lastname
+// console.log(fullname )
+
+// Basic Calculator
+
+// let num1 = 8;
+// let num2 = 10;
+// document.getElementById('num1').textContent = num1;
+// document.getElementById('num2').textContent = num2;
+// let sumOn = document.getElementById('sum')
+
+// function add() {
+//    let result = num1 + num2;
+//    sumOn.textContent = "Sum: " + result
+// }
+// function substract() {
+//    let result = num1 - num2;
+//    sumOn.textContent = "Sum: " + result
+// }
+// function divide() {
+//    let result = num1 / num2;
+//    sumOn.textContent = "Sum: " + result
+// }
+// function multiply() {
+//    let result = num1 * num2;
+//    sumOn.textContent = "Sum: " + result
+// }
+// document.getElementById('scores').innerText = 0;
+// let scores = 0;
+// function addone() {
+//    scores += 1;
+//    document.getElementById('scores').innerText = scores;
+// }
+// function addtwo() {
+//    scores += 2;
+//    document.getElementById('scores').innerText = scores;
+// }                                                                   
+// function addthree() {
+//    scores += 3;
+//    document.getElementById('scores').innerText = scores;
+// }
+
+// document.getElementById('scoresguest').innerText = 0;
+// let scoresguest = 0; 
+// function addon() {
+//    scoresguest += 1;
+//    document.getElementById('scoresguest').innerText = scoresguest;
+// }
+// function addtw() {
+//    scoresguest += 2;
+//    document.getElementById('scoresguest').innerText = scoresguest;
+// }
+// function addthre() {
+//    scoresguest += 3;
+//    document.getElementById('scoresguest').innerText = scoresguest;
+// }  
+
+// function reset() {
+//    scores = 0;
+//    scoresguest = 0;
+//    document.getElementById('scores').innerText = scores;
+//    document.getElementById('scoresguest').innerText = scoresguest;
+// }
